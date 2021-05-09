9aa2db9fbe5866219e1c9f2c9ebc3725af7a2640
youngjun.jin <yzzzzun@gmail.com>
2021-05-09 19:20:08 +0900

    feat: spring으로 전환 스프링 컨테이너에 빈을 등록하고 의존관계주입

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,10 @@
 package hello.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -9,21 +12,26 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
 
+	@Bean
 	public MemberService memberService() {
 		return new MemberServiceImpl(this.memberRepository());
 	}
 
-	private MemberRepository memberRepository() {
+	@Bean
+	public MemberRepository memberRepository() {
 		return new MemoryMemberRepository();
 	}
 
+	@Bean
 	public OrderService orderService() {
 		return new OrderServiceImpl(this.memberRepository(), this.discountPolicy());
 	}
 
-	private DiscountPolicy discountPolicy() {
-		return new FixDiscountPolicy();
+	@Bean
+	public DiscountPolicy discountPolicy() {
+		return new RateDiscountPolicy();
 	}
 }
