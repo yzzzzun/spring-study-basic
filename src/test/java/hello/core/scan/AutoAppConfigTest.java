fde592ce15b89f31a06e9987dbcb1132f04d21f3
youngjun.jin <yzzzzun@gmail.com>
2021-05-11 21:55:33 +0900

    feat: @ComponentScan 의 includeFilter, excludeFilter

--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -3,6 +3,7 @@ package hello.core.scan;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import hello.core.AutoAppConfig;
@@ -12,7 +13,7 @@ public class AutoAppConfigTest {
 
 	@Test
 	void basicScan() {
-		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+		ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
 		MemberService memberServie = ac.getBean(MemberService.class);
 		assertThat(memberServie).isInstanceOf(MemberService.class);
 	}
