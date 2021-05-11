b17e869c9f77ff47bddb2208a06f77164fe18849
youngjun.jin <yzzzzun@gmail.com>
2021-05-11 22:05:59 +0900

    feat: 자동빈 vs 자동빈 등록의 중복, 수동빈 vs 자동빈 등록의 중복 오류가 발생, spring에서는 수동빈을 override하지만 boot에서 옵션으로 오류발생하도록 기본설정되어있음

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,8 +1,12 @@
 package hello.core;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
 
 @Configuration
 @ComponentScan(
@@ -10,4 +14,8 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
+	@Bean(name = "memoryMemberRepository")
+	MemberRepository memberRepository() {
+		return new MemoryMemberRepository();
+	}
 }
