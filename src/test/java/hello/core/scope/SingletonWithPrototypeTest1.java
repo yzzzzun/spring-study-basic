05fd6262dbd17c04cc68bcb49cbd641cb5b1eea7
youngjun.jin <yzzzzun@gmail.com>
2021-05-15 00:22:06 +0900

    test: Provider 자바 표준을 사용해 DL 구현

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -4,9 +4,9 @@ import static org.assertj.core.api.Assertions.*;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -46,10 +46,10 @@ public class SingletonWithPrototypeTest1 {
 	static class ClientBean {
 
 		@Autowired
-		private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+		private Provider<PrototypeBean> prototypeBeanProvider;
 
 		public int logic() {
-			PrototypeBean prototypeBean = this.prototypeBeanProvider.getObject();
+			PrototypeBean prototypeBean = this.prototypeBeanProvider.get();
 			prototypeBean.addCount();
 			return prototypeBean.getCount();
 		}
