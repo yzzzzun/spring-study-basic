d912502150866cfecd6270d398b5ab2a79bd2a8e
youngjun.jin <yzzzzun@gmail.com>
2021-05-14 22:43:52 +0900

    test: @PreConstruct, @PreDestroy 어노테이션 사용하여 초기화, 종료 콜백테스트

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
 	private String url;
@@ -26,11 +29,13 @@ public class NetworkClient {
 		System.out.println("close = " + url);
 	}
 
+	@PreDestroy
 	public void close() {
 		System.out.println("NetworkClient.close");
 		disconnect();
 	}
 
+	@PostConstruct
 	public void init() {
 		System.out.println("NetworkClient.init");
 		connect();
