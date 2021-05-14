a04ae36a999bf3593c31927bb88f63dcb69e352e
youngjun.jin <yzzzzun@gmail.com>
2021-05-14 22:01:12 +0900

    test: spring bean 생명주기 테스트 스프링 빈은 생성후 의존관계 주입이 완료된 후 사용할 준비가 완료됨을 테스트 빈 생성에서 데이터를 초기화했지만 값이 할당되지 않음 확인

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,31 @@
+package hello.core.lifecycle;
+
+public class NetworkClient {
+
+	private String url;
+
+	public NetworkClient() {
+		System.out.println("생성자 호출, url = " + url);
+		connect();
+		call("초기화 연결 메세지");
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	//service 시작시 호출
+	public void connect() {
+		System.out.println("connect = " + url);
+	}
+
+	public void call(String message) {
+		System.out.println("call = " + url + " message = " + message);
+	}
+
+	//service 종료시 호출
+	public void disconnect() {
+		System.out.println("close = " + url);
+	}
+
+}
